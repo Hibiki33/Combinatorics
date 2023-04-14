4b8eb2dab6f234712534817df0d1cafde8c4fef9
Hibiki33 <113701757+Hibiki33@users.noreply.github.com>
2023-04-14 16:02:25 +0800

    Combinatorics

--- combination.py
+++ combination.py
@@ -1,9 +1,9 @@
 import math
 
-n,m = map(int,input().split())
+n, m = map(int,input().split())
 p = 100000007
-def power(x, y):     #求x的y次方
-    p = 100000007
+
+def power(x, y):  
     res = 1
     while y:
         if y % 2 != 0:
@@ -11,8 +11,12 @@ def power(x, y):     #求x的y次方
         y >>= 1
         x *= (x % p)
     return res
-a = (math.factorial(n))%p
-b = (power(math.factorial(m),(p-2))) % p
-c = (power(math.factorial(n-m),(p-2))) % p
-print(a * b * c % p)
+
+if n >= m:
+    a = (math.factorial(n)) % p
+    b = (power(math.factorial(m), (p - 2))) % p
+    c = (power(math.factorial(n - m), (p - 2))) % p
+    print(a * b * c % p)
+else:
+    print("0")
 

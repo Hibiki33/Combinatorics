8bb6f2c40d34ce66c89d3f386b501e6272f24afa
Hibiki33 <113701757+Hibiki33@users.noreply.github.com>
2023-04-19 16:53:22 +0800

    derangement

---
+++ derangement.py
@@ -0,0 +1,16 @@
+'''
+Figure the n'th derangement number
+when n beq 1:
+D(n) = n!(1 - 1/(1!) + 1/(2!) - 1/(3!) + ... + (-1)**n/(n!))
+thus:
+D(n) = (n - 1)(D(n - 2) + D(n - 1))
+D(n) = nD(n - 1) + (-1)**n 
+'''
+
+n = int(input())
+d = 1
+
+for i in range(1, n + 1):
+    d = i * d + (-1)**i
+
+print(d)
